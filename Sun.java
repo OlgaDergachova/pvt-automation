42014ddd3b7155e2f1653f9cb218cb94c0f016f9
OlgaDergachova <dergachova.olya@gmail.com>
2018-07-16 21:16:46 +0300

    fourth commit

---
+++ Sun.java
@@ -0,0 +1,10 @@
+package PackagePlanets;
+
+public class Sun extends Star {
+
+	int distance;
+	
+	public void getFullStarInfo() {
+		System.out.println(super.systemName + " " + super.starName);
+	}
+}
