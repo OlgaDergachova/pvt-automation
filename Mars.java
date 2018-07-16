7d4b9a1a8b741239c1f4e56484bf72635a3a9e8b
OlgaDergachova <dergachova.olya@gmail.com>
2018-07-16 21:12:28 +0300

    second commit

---
+++ Mars.java
@@ -0,0 +1,11 @@
+package PackagePlanets;
+
+public class Mars extends Planet{
+	
+	int radius;
+	
+	public void getFullPlanetInfo() {
+		System.out.println(super.systemName + " " + super.planetName);
+	}
+
+}
---
+++ Sirius.java
@@ -0,0 +1,11 @@
+package PackagePlanets;
+
+public class Sirius extends Star{
+	
+	double power;
+	
+	public void getFullStarInfo() {
+		System.out.println(super.systemName + " " + super.starName);
+	}
+
+}
